0a81830545a2b4a0567150067ab965a91d23656e
Marianne <marianne.siren@gmail.com>
2020-02-17 17:31:41 +0200

    Add hovering to map markers

--- citi-bike-history.py
+++ citi-bike-history.py
@@ -1,20 +1,27 @@
 from bokeh.plotting import output_file, show
-from bokeh.models import ColumnDataSource, GMapOptions
+from bokeh.models import ColumnDataSource, GMapOptions, HoverTool
 from bokeh.plotting import gmap
 from config import *
 import pandas as pd
 
 df = pd.read_csv('citi-bike-history-data.csv',
-                 usecols=["start station id", "start station latitude", "start station longitude"]
+                 usecols=["start station name", "start station id", "start station latitude", "start station longitude"]
                  )
 
-# Seems that values are always ordered in tuples, so we can for now trust that the id is the first value
-# in tuple, latitude is always the second value in tuple and longitude is the third value in tuple
+# Create tuples from dataframe
 geocode_tuples = [tuple(x) for x in df.to_numpy()]
+
+# Remove duplicates
 uniq_geocodes = list(set([i for i in geocode_tuples]))
 
-latitudes = [i[1] for i in uniq_geocodes]
-longitudes = [i[2] for i in uniq_geocodes]
+# Seems that values are always ordered in tuples, so we can for now trust that
+# - the of the station is the first value
+# - id is the second value
+# - latitude is always the third value
+# - longitude is the fourth value in tuple
+station_name = [i[0] for i in uniq_geocodes]
+latitudes = [i[2] for i in uniq_geocodes]
+longitudes = [i[3] for i in uniq_geocodes]
 
 output_file("gmap.html")
 
@@ -24,8 +31,13 @@ p = gmap(GOOGLE_MAPS_API_KEY, map_options, title="Start stations")
 
 source = ColumnDataSource(
     data=dict(lat=latitudes,
-              lon=longitudes)
+              lon=longitudes,
+              name=station_name)
 )
+
+hover = HoverTool()
+hover.tooltips = [('Name of the station', '@name')]
+p.add_tools(hover)
 
 p.circle(x="lon", y="lat", size=10, fill_color="tomato", fill_alpha=0.8, source=source)
 
