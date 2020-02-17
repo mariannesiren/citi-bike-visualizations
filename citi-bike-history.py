3685cc94a11fe9c38639f61e9adafa3f3096e79d
Marianne <marianne.siren@gmail.com>
2020-02-17 17:48:33 +0200

    Add end stations to used data

--- citi-bike-history.py
+++ citi-bike-history.py
@@ -4,30 +4,49 @@ from bokeh.plotting import gmap
 from config import *
 import pandas as pd
 
-df = pd.read_csv('citi-bike-history-data.csv',
-                 usecols=["start station name", "start station id", "start station latitude", "start station longitude"]
-                 )
+df_start_stations = pd.read_csv('citi-bike-history-data.csv',
+                 usecols=[
+                     "start station name",
+                     "start station id",
+                     "start station latitude",
+                     "start station longitude"
+                 ])
 
-# Create tuples from dataframe
-geocode_tuples = [tuple(x) for x in df.to_numpy()]
+df_end_stations = pd.read_csv('citi-bike-history-data.csv',
+                 usecols=[
+                     "end station name",
+                     "end station id",
+                     "end station latitude",
+                     "end station longitude"
+                 ])
+
+
+# Create tuples from start station dataframe
+tuples_start_stations = [tuple(x) for x in df_start_stations.to_numpy()]
+
+# Create tuples from end station dataframe
+tuples_end_stations = [tuple(x) for x in df_end_stations.to_numpy()]
+
+# Join start station tuples and end station tuples
+station_tuples = tuples_end_stations + tuples_start_stations
 
 # Remove duplicates
-uniq_geocodes = list(set([i for i in geocode_tuples]))
+uniq_stations = list(set([i for i in station_tuples]))
 
 # Seems that values are always ordered in tuples, so we can for now trust that
 # - the of the station is the first value
 # - id is the second value
 # - latitude is always the third value
 # - longitude is the fourth value in tuple
-station_name = [i[0] for i in uniq_geocodes]
-latitudes = [i[2] for i in uniq_geocodes]
-longitudes = [i[3] for i in uniq_geocodes]
+station_name = [i[0] for i in uniq_stations]
+latitudes = [i[2] for i in uniq_stations]
+longitudes = [i[3] for i in uniq_stations]
 
 output_file("gmap.html")
 
 map_options = GMapOptions(lat=40.73, lng=-74.05, map_type="roadmap", zoom=14)
 
-p = gmap(GOOGLE_MAPS_API_KEY, map_options, title="Start stations")
+p = gmap(GOOGLE_MAPS_API_KEY, map_options, title="Used stations in December 2019")
 
 source = ColumnDataSource(
     data=dict(lat=latitudes,
