450155018490597b8af38a6a8bdce048789e897a
Marianne <marianne.siren@gmail.com>
2020-02-18 18:24:09 +0200

    Add awesome slider

--- citi-bike-history-by-station.py
+++ citi-bike-history-by-station.py
@@ -1,5 +1,6 @@
+from bokeh.layouts import column
 from bokeh.plotting import output_file, show, figure
-from bokeh.models import HoverTool
+from bokeh.models import HoverTool, Slider
 import pandas as pd
 
 df = pd.read_csv('citi-bike-history-data.csv', usecols=["tripduration", "start station name"])
@@ -9,12 +10,14 @@ grouped_data = df.groupby('start station name')['tripduration'].sum().to_frame()
 output_file("citi-bike-history-by-station.html")
 
 p = figure(plot_width=1000, plot_height=500, x_range=grouped_data['start station name'])
-p.circle(grouped_data['start station name'], grouped_data['tripduration'] % 60, size=10, color="tomato", alpha=0.5)
-p.y_range.start = 0
+r = p.circle(grouped_data['start station name'], grouped_data['tripduration'] % 60, color="tomato", alpha=0.5)
 p.xaxis.major_label_orientation = 1.2
 
 hover = HoverTool()
 hover.tooltips = [('Duration', '@y')]
 p.add_tools(hover)
 
-show(p)
+slider = Slider(start=0.1, end=2, step=0.01, value=0.3)
+slider.js_link('value', r.glyph, 'radius')
+
+show(column(p, slider))
